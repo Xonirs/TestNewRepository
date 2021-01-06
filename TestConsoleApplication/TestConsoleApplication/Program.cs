3951f341fa0bd3805fd6f1f8214895ccd519f207
Xonirs <66732623+Xonirs@users.noreply.github.com>
2021-01-06 19:23:54 +0200

    Added new method, brainteaser nr.5 inspiration

--- TestConsoleApplication/TestConsoleApplication/Program.cs
+++ TestConsoleApplication/TestConsoleApplication/Program.cs
@@ -8,11 +8,25 @@ namespace TestConsoleApplication
         {
             Console.WriteLine("Hello World!");
             DoStuff();
+            DoAnotherStuff();
         }
 
         static void DoStuff()
         {
             Console.WriteLine("bananas");
+        }
+
+        static void DoAnotherStuff()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write($"{i * j} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
     }
 }
