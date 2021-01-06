e62ff6655e0f83df84990bf25fe998e84b4cc484
Xonirs <66732623+Xonirs@users.noreply.github.com>
2021-01-06 19:21:20 +0200

    initial commit. creating solution
    test description. Added also method DoStuff()

---
+++ TestConsoleApplication/TestConsoleApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DoStuff();
+        }
+
+        static void DoStuff()
+        {
+            Console.WriteLine("bananas");
+        }
+    }
+}
